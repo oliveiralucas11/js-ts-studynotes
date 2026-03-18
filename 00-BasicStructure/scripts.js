700a9a5d7d52bf9f3f1bada8e5ea4f59d6ad4a37
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2026-03-18 15:57:51 -0400

    feat: estrutura basica de conexao de js com o html

---
+++ 00-BasicStructure/scripts.js
@@ -0,0 +1,6 @@
+console.log("Hello World")
+console.log("Essa é uma mensagem do js conectado com o html")
+
+// Esse é um tipo de comentario em javascript e em qualquer linguagem de programação
+
+/* Comentario usado para multiplas linhas em javascript ou em qualquer linguagem de programação */
